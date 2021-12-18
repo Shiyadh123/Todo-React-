7fc01a3cc76236e932d0a0443be6479751bf4765
Shiyadh123 <74063199+Shiyadh123@users.noreply.github.com>
2021-12-18 08:26:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoEdit from "./components/TodoEdit";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [editingMode, setEditingMode] = useState(null);
+
+  const addTodoHandler = (newTodoItem) => {
+    setTodoList((prev) => {
+      return [
+        ...prev,
+        {
+          id: prev.length,
+          value: newTodoItem,
+          isDone: false,
+        },
+      ];
+    });
+  };
+
+  const deleteTodoHandler = (id) => {
+    const newItems = todoList.filter((item) => item.id !== id);
+    setTodoList(newItems);
+  };
+
+  const editTodoHandler = (id) => {
+    setEditingMode(id);
+  };
+
+  const updateTodoHandler = (updatedItem) => {
+    const newItems = todoList.map((item) => {
+      return item.id === updatedItem.id ? updatedItem : item;
+    });
+    setTodoList(newItems);
+    setEditingMode(null);
+  };
+
+  const isDoneHandler = (id) => {
+    const newItems = todoList.map((item) => {
+      return item.id === id ? { ...item, isDone: !item.isDone } : item;
+    });
+    setTodoList(newItems);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {editingMode !== null ? (
+        <TodoEdit
+          todoListItem={todoList[editingMode]}
+          updateTodoHandler={updateTodoHandler}
+        ></TodoEdit>
+      ) : (
+        <TodoInput addTodoHandler={addTodoHandler}></TodoInput>
+      )}
+
+      <TodoList
+        todoList={todoList}
+        deleteTodoHandler={deleteTodoHandler}
+        editTodoHandler={editTodoHandler}
+        isDoneHandler={isDoneHandler}
+      ></TodoList>
+    </Fragment>
   );
 }
 
